f5b304a964a6bdd25aa2ce6db895b380c14efd78
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-04-27 08:17:08 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+const writeCards = array => array.map((e)=> `Thank you, ${e}, for the wonderful surprise gift!`)
+
+const countDown = (n) => {
+    var i = n
+
+    while (i>=0) {
+        console.log(i)
+        i-=1
+        
+    }
+
+}
